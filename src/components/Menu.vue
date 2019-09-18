7c5f6fbf56c4d5db76678f744639a686c151a158
Alex Liu <sotework@outlook.com>
2019-09-18 19:12:06 +0800

    初始化项目

---
+++ src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Menu">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+<script>
+  import Vue from 'vue';
+  export default class Menu extends Vue {
+    name = 'Menu';
+  }
+</script>
+<style scoped>
+  .Menu {
+    padding: 30px;
+  }
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  a.router-link-exact-active {
+    color: #42b983;
+  }
+</style>
