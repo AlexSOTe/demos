5615c553e213c3a8a8ca4138f9c8b0419bd8cd46
Alex Liu <sotework@outlook.com>
2019-10-29 16:25:02 +0800

    修改

--- src/js/threejsDemo.js
+++ src/js/threejsDemo.js
@@ -2,60 +2,58 @@ import * as THREE from 'three';
 import store from '@/store/store';
 
 export default function ThreeDemo(el) {
-  let obj = {
-    scene: null,
-    camera: null,
-    renderer: null,
-    ctnr: el,
-    ctnrW: 0,
-    ctnrH: 0,
-    light: null,
-    cube: null
-  };
+  let scene = null;
+  let camera = null;
+  let renderer = null;
+  let ctnr = el;
+  let ctnrW = 0;
+  let ctnrH = 0;
+  let light = null;
+  let cube = null;
 
   function InitCtnr() {
-    obj.ctnrW = parseInt(getComputedStyle(obj.ctnr).width);
-    obj.ctnrH = parseInt(getComputedStyle(obj.ctnr).height);
+    ctnrW = parseInt(getComputedStyle(ctnr).width);
+    ctnrH = parseInt(getComputedStyle(ctnr).height);
   }
   function InitScene() {
-    obj.scene = new THREE.Scene();
-    obj.scene.name = 'Main';
-    obj.scene.background = new THREE.Color(0x1e1e1e);
+    scene = new THREE.Scene();
+    scene.name = 'Main';
+    scene.background = new THREE.Color(0x1e1e1e);
   }
   function InitRenderer() {
-    obj.renderer = new THREE.WebGLRenderer({ antialias: true });
-    obj.renderer.setPixelRatio(window.devicePixelRatio);
-    obj.renderer.setSize(obj.ctnrW, obj.ctnrH);
-    obj.renderer.domElement.style.display = 'block';
-    obj.ctnr.appendChild(obj.renderer.domElement);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(ctnrW, ctnrH);
+    renderer.domElement.style.display = 'block';
+    ctnr.appendChild(renderer.domElement);
   }
   function InitCamera() {
-    obj.camera = new THREE.PerspectiveCamera(103, obj.ctnrW / obj.ctnrH, 0.1, 2000);
-    obj.camera.position.set(100, 0, 100);
-    obj.camera.lookAt(new THREE.Vector3(0, 0, 0));
-    obj.scene.add(obj.camera);
+    camera = new THREE.PerspectiveCamera(103, ctnrW / ctnrH, 0.1, 2000);
+    camera.position.set(100, 0, 100);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    scene.add(camera);
   }
   function InitLight() {
-    obj.light = new THREE.DirectionalLight(0xffffff, 1.5);
-    obj.light.position.set(500, 500, 500);
-    obj.scene.add(obj.light);
+    light = new THREE.DirectionalLight(0xffffff, 1.5);
+    light.position.set(500, 500, 500);
+    scene.add(light);
   }
   function InitCube() {
     let cubeGeometry = new THREE.CubeGeometry(50, 50, 50, 50, 50, 50);
     let cubeMaterial = new THREE.MeshLambertMaterial({
       color: 0xff0000
     });
-    obj.cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    obj.cube.position.set(0, 0, 0);
-    obj.scene.add(obj.cube);
+    cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.set(0, 0, 0);
+    scene.add(cube);
   }
 
   function Render() {
     console.log('Rendering');
-    obj.renderer.render(obj.scene, obj.camera);
+    renderer.render(scene, camera);
 
-    obj.cube.rotation.x += 0.005;
-    obj.cube.rotation.y += 0.01;
+    cube.rotation.x += 0.005;
+    cube.rotation.y += 0.01;
 
     let animationHandle = requestAnimationFrame(Render);
     store.commit('SetAnimation', animationHandle);
@@ -68,6 +66,4 @@ export default function ThreeDemo(el) {
   InitCube();
   InitRenderer();
   Render();
-
-  return obj;
 }
