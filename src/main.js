4944e379cbb7f5fc2a16e791a0601cf0dcb29ae8
Alex Liu <sotework@outlook.com>
2019-10-31 13:56:09 +0800

    modify

--- src/main.js
+++ src/main.js
@@ -5,12 +5,10 @@ import store from './store/store';
 import './registerServiceWorker';
 import Vant from 'vant';
 import 'vant/lib/index.css';
-import * as THREE from 'three';
 
 Vue.config.productionTip = false;
 
 Vue.use(Vant);
-Vue.prototype.$3 = THREE;
 
 new Vue({
   router,
